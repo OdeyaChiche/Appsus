3b23af10b60074e25808b7bad0ebb4581c122a45
odeyasl@gmail.com <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2024-10-09 19:12:07 +0300

    create add note input

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -7,6 +7,14 @@ export function NoteIndex() {
   const [notes, setNotes] = useState(noteService.query())
   console.log(notes)
 
+  const [isExpanded, setIsExpanded] = useState(false)
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const expandNote = () => {
+    setIsExpanded(true)
+  }
+
   function onRemoveNote(noteId) {
     console.log('removed')
   }
@@ -17,9 +25,28 @@ export function NoteIndex() {
 
   if (!notes) return <h1>Loading...</h1>
 
-  //    return <div>note app</div>
   return (
     <section className='note-index'>
+      <div className='add-note'>
+        {isExpanded && (
+          <input
+            type='text'
+            placeholder='Title'
+            value={title}
+            onChange={(ev) => setTitle(ev.target.value)}
+            className='add-note-title'
+          />
+        )}
+
+        <input
+          type='text'
+          placeholder='Add note...'
+          value={body}
+          onFocus={expandNote}
+          onChange={(ev) => setBody(ev.target.value)}
+          className='add-note-body'
+        />
+      </div>
       <NoteList notes={notes} />
     </section>
   )
