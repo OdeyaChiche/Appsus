a0c043f456734f72f8440ed5a098ab11830677ed
odeyasl@gmail.com <YOUR_EMAIL_ADDRESS@EXAMPLE.COM>
2024-10-13 18:15:10 +0300

    make the checkbox managable

--- apps/note/cmps/NoteTodos.jsx
+++ apps/note/cmps/NoteTodos.jsx
@@ -1,3 +1,5 @@
+
+
 export function NoteTodosPreview({ info }) {
   return (
     <article className='note todos-preview'>
@@ -5,8 +7,8 @@ export function NoteTodosPreview({ info }) {
         {info.todos.map((todo, index) => (
           <li key={index}>
             <div>
-              <input type='checkbox' checked={todo.doneAt} id="todo" />
-              <label for="todo">{todo.txt}</label>
+              <input type='checkbox' defaultChecked={todo.doneAt}  id="todo" />
+              <label htmlFor="todo">{todo.txt}</label>
            </div>
           </li>
         ))}
